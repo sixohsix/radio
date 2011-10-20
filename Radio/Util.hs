177bbca0433d282aa14492edf9266c4a16f87f06
miv <mike.verdone@ableton.com>
2011-10-20 14:51:28 +0200

    Func to get oldest file in a dir.

---
+++ Radio/Util.hs
@@ -0,0 +1,17 @@
+
+module Radio.Util where
+
+import Control.Monad (filterM)
+import List (sortBy)
+import System.Directory (getDirectoryContents, getModificationTime, doesFileExist)
+import System.Cmd (rawSystem)
+
+
+oldestFileInDir :: FilePath -> IO FilePath
+oldestFileInDir dir = do 
+  dirContents <- getDirectoryContents dir
+  dirContents <- filterM doesFileExist dirContents
+  dirContents <- return $ filter (\fn -> head fn /= '.') dirContents  -- Ignore hidden files
+  modTimes <- sequence $ map (\fp -> getModificationTime fp) dirContents
+  return $ fst $ head $ sortBy orderModTime $ zip dirContents modTimes where
+    orderModTime (a,ma) (b,mb) = compare ma mb
